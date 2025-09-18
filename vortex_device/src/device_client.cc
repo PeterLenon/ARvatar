9790a5694e87d46a8da991e36d5c53b1ab936d99
PeterLenon <goshomip@gmail.com>
2025-09-18 18:20:58 +0000

    Refactor gRPC protos for asset and dialogue services

--- vortex_device/src/device_client.cc
+++ vortex_device/src/device_client.cc
@@ -3,8 +3,8 @@
 #include <vector>
 #include <cstdint>
 #include "voxel/common/v1/types.pb.h"
-#include "voxel/ingest/v1/ingest_service.grpc.pb.h"
-#include "voxel/recon/v1/recon_service.grpc.pb.h"
+#include "voxel/asset/v1/asset_service.grpc.pb.h"
+#include "voxel/dialogue/v1/dialogue_service.grpc.pb.h"
 
 int main(int argc, char** argv) {
   const std::string target = (argc > 1) ? argv[1] : "localhost:50051";
@@ -13,17 +13,25 @@ int main(int argc, char** argv) {
   auto channel = grpc::CreateChannel(target, grpc::InsecureChannelCredentials());
 
   // Stubs (note variable names avoid shadowing namespaces)
-  auto ingest_stub = voxel::ingest::v1::IngestService::NewStub(channel);
-  auto recon_stub  = voxel::recon::v1::ReconService::NewStub(channel);
+  auto asset_stub    = voxel::assets::v1::AssetService::NewStub(channel);
+  auto dialogue_stub = voxel::dialogue::v1::DialogueService::NewStub(channel);
 
-  // Build a sample message to verify models compile/link
-  voxel::common::v1::FrameChunk chunk;
-  chunk.set_codec("RAW");
-  chunk.set_width(640);
-  chunk.set_height(480);
-  chunk.set_timestamp_ms(0);
-  static const char kDummy[] = "dummy";
-  chunk.set_data(kDummy, sizeof(kDummy)-1);
+  // Build sample messages to verify models compile/link with the refactored protos
+  voxel::assets::v1::GetPointCloudRequest asset_request;
+  asset_request.set_guru_id("demo-guru");
+  asset_request.set_variant("neutral");
+  asset_request.set_include_mesh(true);
+
+  voxel::dialogue::v1::AskRequest ask_request;
+  ask_request.set_guru_id("demo-guru");
+  ask_request.set_user_query("Hello Guru!");
+  ask_request.add_context_tags("intro");
+  ask_request.set_return_audio(false);
+  ask_request.set_return_lipsync(true);
+
+  voxel::common::v1::GuruProfile profile;
+  profile.set_guru_id("demo-guru");
+  profile.set_display_name("Demo Guru");
 
   std::cout << "Device client compiled. Channel to " << target << " created.\n";
   return 0;
